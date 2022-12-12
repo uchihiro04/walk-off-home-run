bf07497e37b6e31f6936129549f5642401b8e188
uchihiro <tohchi4122@gmail.com>
2022-12-12 17:13:17 +0900

    一部コードにコメントを挿入
    一部コードの意図を補足するために、コメントを挿入しました。

--- main.js
+++ main.js
@@ -2,5 +2,6 @@
 
 const Command = require("./lib/command.js");
 const argv = require("minimist")(process.argv.slice(2));
+// コマンドのオプションに関するオブジェクトである変数argvから、不必要なプロパティを削除
 delete argv._;
 new Command(argv).runGame();
